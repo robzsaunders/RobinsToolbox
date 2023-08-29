ebac30a1f7302c321ce58f0ce69be496e3b4e5fe
robzsaunders <robz.saunders@gmail.com>
2023-08-29 14:36:43 -0400

    Changing over groupId to fit maven

--- src/main/java/io/github/robzsaunders/robinstoolbox/Print.java
+++ src/main/java/io/github/robzsaunders/robinstoolbox/Print.java
@@ -21,7 +21,7 @@
  * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
  * THE SOFTWARE.
  */
-package com.tendiesinc.robinstoolbox;
+package io.github.robzsaunders.robinstoolbox;
 
 import java.io.BufferedWriter;
 import java.io.File;
